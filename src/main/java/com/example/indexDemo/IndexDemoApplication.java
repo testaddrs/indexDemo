66946204bc85c5e2306e51fe1d65e00719f6ad37
siwon <tkxkdcheld@naver.com>
2022-11-09 10:16:21 +0900

    Initial commit

---
+++ src/main/java/com/example/indexDemo/IndexDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.indexDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndexDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/indexDemo/IndexDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.indexDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
